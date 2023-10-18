2db691c7bd065905fd097097a9e7fba99f33297c
Dasher <55836007+the1andonlyDasher@users.noreply.github.com>
2023-10-18 14:07:05 +0200

    ESLint

--- src/components/Navbar/Navigation.tsx
+++ src/components/Navbar/Navigation.tsx
@@ -1,25 +1,25 @@
 import React from "react";
 import { motion } from "framer-motion";
-import {NavItem} from "./NavItemDesktop";
+import { NavItem } from "./NavItemDesktop";
 
 const variants = {
-    open: {
-      transition: { staggerChildren: 0.07, delayChildren: 0.2 }
-    },
-    closed: {
-      transition: { staggerChildren: 0.05, staggerDirection: -1 }
-    }
-  };
+  open: {
+    transition: { staggerChildren: 0.07, delayChildren: 0.2 }
+  },
+  closed: {
+    transition: { staggerChildren: 0.05, staggerDirection: -1 }
+  }
+};
 
 
- 
 
 
-const Navigation = ({children}) => {
+
+const Navigation = ({ children }: any) => {
   return (
     <>
       <motion.ul className="nav-items-desktop" variants={variants}>
-      {children}
+        {children}
       </motion.ul>
     </>
   );
