345f5e7e7fbd5d377573fe9295118e7fb087d023
Dasher <55836007+the1andonlyDasher@users.noreply.github.com>
2023-10-18 19:00:24 +0200

    changed about anims to pure css

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -7,38 +7,38 @@ import Link from "next/link";
 import Footer from "@/components/Footer";
 
 const cards_variants = {
-  initial: { opacity: 0, scale: 0 },
-  enter: { opacity: 1, scale: 1, transition: { staggerChildren: 0.2, when: "beforeChildren" } },
-  exit: { opacity: 0, scale: 0, transition: { staggerChildren: 0.2, when: "afterChildren" } },
-}
+  initial: { opacity: 0 },
+  enter: {
+    opacity: 1,
+    transition: { staggerChildren: 0.2, when: "beforeChildren" },
+  },
+  exit: {
+    opacity: 0,
+    transition: { staggerChildren: 0.1, when: "afterChildren" },
+  },
+
+};
 
 const card_variants = {
   initial: { opacity: 0, scale: 0 },
   enter: { opacity: 1, scale: 1 },
   exit: { opacity: 0, scale: 0 },
-}
+
+};
 
 const button_variants = {
   hidden: { y: 20, opacity: 0 },
-  visible: { y: -20, opacity: 1 }
-}
+  visible: { y: -20, opacity: 1 },
+};
 
 const name_variants = {
   hidden: { y: 0, opacity: 1 },
-  visible: { y: 20, opacity: 0 }
-}
+  visible: { y: 20, opacity: 0 },
+};
 
 export default function About() {
   const [tapped, setTapped] = useState(false);
-  const Cards = (images.map((img: any, index: number) => {
-    const controls = useAnimation();
-    return (
-      <motion.div onTap={() => { setTapped(!tapped), controls.start(tapped ? "visible" : "hidden") }} onMouseEnter={() => controls.start("visible")} onMouseLeave={() => controls.start("hidden")} variants={card_variants} className="card" key={index}>
-        <motion.div className="img" style={{ backgroundImage: `url(${img.url})`, clipPath: `${img.clip}` }}></motion.div>
-        <motion.div className="name" initial="hidden" animate={controls} variants={name_variants}><h4>{img.creator}</h4></motion.div>
-        <motion.div className="name" initial="hidden" animate={controls} variants={button_variants}><Link href={`/about/${img.id}`}><h5>Read more</h5></Link></motion.div>
-      </motion.div>)
-  }))
+
   return (
     <>
       <Head>
@@ -47,12 +47,38 @@ export default function About() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.webp" />
       </Head>
-      <Sec children={(
+      <Sec>
         <motion.div variants={cards_variants} className="cards">
-          {Cards}
+          {images.map((img: any, index: number) => (
+            <motion.div
+              variants={card_variants}
+              className="card"
+              key={index}
+            >
+              <motion.div
+                className="img"
+                style={{
+                  backgroundImage: `url(${img.url})`,
+                  clipPath: `${img.clip}`,
+                }}
+              ></motion.div>
+              <motion.div
+                className="name"
+              >
+                <h4>{img.creator}</h4>
+              </motion.div>
+              <motion.div
+                className="button"
+              >
+                <Link href={`/about/${img.id}`}>
+                  <h5>Read more</h5>
+                </Link>
+              </motion.div>
+            </motion.div>
+          ))}
         </motion.div>
-      )} />
+      </Sec>
       <Footer />
     </>
-  )
+  );
 }
