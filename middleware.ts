ff58be84a637cfbf30f882cee13601880f872d31
Dasher <55836007+the1andonlyDasher@users.noreply.github.com>
2023-10-20 23:02:35 +0200

    updated cors

--- middleware.ts
+++ middleware.ts
@@ -12,7 +12,10 @@ export function middleware() {
 
   // add the CORS headers to the response
   res.headers.append("Access-Control-Allow-Credentials", "true");
-  res.headers.append("Access-Control-Allow-Origin", "*"); // replace this your actual origin
+  res.headers.append(
+    "Access-Control-Allow-Origin",
+    "https://rulezero-ts.vercel.app/"
+  ); // replace this your actual origin
   res.headers.append(
     "Access-Control-Allow-Methods",
     "GET,DELETE,PATCH,POST,PUT"
