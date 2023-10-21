6f249d97eef31f9c92bf6bdef9bd67c6c78b73b0
Dasher <55836007+the1andonlyDasher@users.noreply.github.com>
2023-10-21 15:07:12 +0200

    cors

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -1,6 +1,6 @@
 import NextCors from "nextjs-cors";
 
-async function handler(req: any, res: any) {
+export async function handler(req: any, res: any) {
   // Run the cors middleware
   // nextjs-cors uses the cors package, so we invite you to check the documentation https://github.com/expressjs/cors
   await NextCors(req, res, {
