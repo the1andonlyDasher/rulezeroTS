e24532cf37507a4eb7362343862a69ecd2b2fe56
Dasher <55836007+the1andonlyDasher@users.noreply.github.com>
2023-10-21 17:24:09 +0200

    cors

--- next.config.js
+++ next.config.js
@@ -4,9 +4,30 @@ const ThreeMinifierPlugin = require("@yushijinhun/three-minifier-webpack");
 const nextConfig = {
   reactStrictMode: true,
   compress: true,
-  output: "export",
+
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
   },
   webpack: (config, { isServer, dev }) => {
     if (!isServer && !dev) {
@@ -37,3 +58,4 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 });
 
 module.exports = withBundleAnalyzer(withPWA(nextConfig));
+// module.exports = withBundleAnalyzer(nextConfig);
