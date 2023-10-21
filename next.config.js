967f2ef6afab299786f4dd42da9edc32d3a9fd73
Dasher <55836007+the1andonlyDasher@users.noreply.github.com>
2023-10-21 14:37:11 +0200

    changed middleware location

--- next.config.js
+++ next.config.js
@@ -4,33 +4,9 @@ const ThreeMinifierPlugin = require("@yushijinhun/three-minifier-webpack");
 const nextConfig = {
   reactStrictMode: true,
   compress: true,
-  swcMinify: true,
+  output: "export",
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
-  },
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/_next/:path",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "https://rulezero-ts.vercel.app",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-UserSession",
-          },
-        ],
-      },
-    ];
   },
   webpack: (config, { isServer, dev }) => {
     if (!isServer && !dev) {
